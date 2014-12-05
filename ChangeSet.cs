064e5cd1dbf359f7d1716c88cd69cc697cb1c5a8
Ben Peddell <klightspeed@killerwolves.net>
2014-12-05 12:29:25 +1000

    Add ChangeSet and IChangeTrackingLASDTable

---
+++ ChangeSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace TSVCEO.LASDDatabase
+{
+    public class EntryModification
+    {
+        public string EntryID { get; set; }
+        public bool MoveEntry { get; set; }
+        public Entry Old { get; set; }
+        public Entry New { get; set; }
+        public Entry OldInsertBefore { get; set; }
+        public Entry NewInsertBefore { get; set; }
+    }
+
+    public class ChangeSet
+    {
+        public string StackTrace { get; set; }
+        public string ChangeSetID { get; set; }
+        public ChangeSet RevertFrom { get; set; }
+        public ChangeSet ParentChangeSet { get; set; }
+        public string OldAssessmentName { get; set; }
+        public string NewAssessmentName { get; set; }
+        public string OldAssessmentPurpose { get; set; }
+        public string NewAssessmentPurpose { get; set; }
+        public List<EntryModification> Modifications { get; set; }
+
+        public ChangeSet()
+        {
+            Modifications = new List<EntryModification>();
+            this.ChangeSetID = Guid.NewGuid().ToString();
+            this.StackTrace = new StackTrace().ToString();
+        }
+    }
+}
---
+++ IChangeTrackingLASDTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TSVCEO.LASDDatabase;
+
+namespace GTMJ_Creator.Backend
+{
+    public interface IChangeTrackingLASDTable : ILASDTable
+    {
+        ChangeSet GetLastChangeSet();
+        IEnumerable<ChangeSet> GetAllChangeSets();
+        bool RevertToChangeSet(string revertto);
+    }
+}
